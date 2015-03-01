83ed33c9d7b33393dfc3b2378d73d8ca4304a717
RanjeetR <ranjeet.hange@outlook.com>
2015-03-01 21:48:57 +0530

    Basic angular

--- client/js/controller/collectionController.js
+++ client/js/controller/collectionController.js
@@ -1,4 +1,15 @@
 angular.module('app').controller('collectionController', function ($scope) {
 $scope.intCount = 20;
 console.log("collectionController ");
+    $scope.collection = [{
+        fruits: "apple",
+        color:"green"
+    } ];
+    $scope.savedata = function () {
+        $scope.collection.push({
+            fruits:$scope.fruitname,
+            color:$scope.fruitcolor
+        });
+       // $scope.collection.push({color:$scope.fruitcolor});
+    }
 });
